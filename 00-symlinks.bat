f82a188d5908f861eb813d6b62526cccdcb9158d
breadcat <sendforththegunboats@gmail.com>
2013-08-19 17:51:52 +0100

    Complete the set
    Added missing channels
    Renumbered to match category blocks
    Single line <rule><value> equivalents
    Added last played rules

--- 00-symlinks.bat
+++ 00-symlinks.bat
@@ -2,10 +2,12 @@ rem symlink xbmc channels playlist directory to git
 rem will ruthlesly delete existing directories, so be sure to backup anything you want to keep
 rem alter variables where necessary
 
-set dir=%userprofile%\Documents\GitHub\xbmc-pseudotvchannels
+set dir=%userprofile%\Dropbox\git\xbmc-pseudotvchannels
 set xbmc=%appdata%\XBMC\userdata\playlists\video
 
 if exist "%xbmc%" rd /s /q "%xbmc%"
 mklink /D "%xbmc%" "%dir%"
 
 pause
+exit
+
